ef14114ff802d577e234aba5bda78a0925a913c9
club789456123 <isaacismaelx14@gmail.com>
2021-05-19 14:00:23 -0400

    Creating addin layaout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routers/App.router";
 import AuthProvider from "./auth/Auht.provider";
+import Layout from "./components/layouts/layout";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Router>
-          <AppRouter />
-        </Router>
+        <Layout>
+          <Router>
+            <AppRouter />
+          </Router>
+        </Layout>
       </AuthProvider>
     </div>
   );
